6de3493e3797b8487cebad54e9f5d6816d2013d2
Peter Gad <petergad14@gmail.com>
2023-07-22 10:50:48 +0300

    Update SCK_control.v

--- SCK_control/SCK_control.v
+++ SCK_control/SCK_control.v
@@ -3,10 +3,21 @@ module SCK_control(
     input wire M_BaudRate,
     input wire CPOL,
     input wire CPHA,
-    output wire SCK_out,
-    output wire Shift_clk,
-    output wire Sample_clk
+    input wire idle, 
+    output reg SCK_out,
+    output reg Shift_clk,
+    output reg Sample_clk
 );
 
+always @(*) begin
+    Shift_clk = !idle & !M_BaudRate;
+    Sample_clk = M_BaudRate;
+    case({CPOL, CPHA})
+    0: SCK_out = M_BaudRate;
+    1: SCK_out = !idle & !M_BaudRate;
+    2: SCK_out = !M_BaudRate;
+    3: SCK_out = idle | M_BaudRate;
+    endcase
+end
 
 endmodule

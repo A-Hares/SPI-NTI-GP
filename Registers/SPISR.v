4685001090fc7413befb2336bfdd387c1639544e
Peter Gad <139645814+petergad14@users.noreply.github.com>
2023-07-22 15:16:48 +0300

    Update SPISR.v

--- Registers/SPISR.v
+++ Registers/SPISR.v
@@ -2,22 +2,20 @@
 
 module SPISR(
     input wire clk, rst, en, 
-    input wire [7:0] SPISR_in,
-    output reg SPIF, SPTEF
+    input wire SPISR_in,
+    output reg SPIF
 );
 
 always @(posedge clk or negedge rst) begin
     if(~rst)begin
-      SPIF <= 0; SPTEF <= 0;
+      SPIF <= 0;
     end
     else begin
         if(en) begin
-          SPIF <= SPISR_in[7];
-          SPTEF <= SPISR_in[5];
+          SPIF <= SPISR_in;
         end
         else begin
           SPIF <= SPIF;
-          SPTEF <= SPTEF;
         end
       
     end

54ef3d0994abaa2a431de9d0a79a231836ea14b1
A-Hares <ahmed.haris2001@gmail.com>
2023-07-22 14:26:23 +0300

    Shifter update

--- Shifter/Shifter.v
+++ Shifter/Shifter.v
@@ -1,3 +1,4 @@
+
 `default_nettype none
 module Shifter #(parameter DWIDTH = 8)(
     input wire Sample_clk, // to take input from data in (from slave)
@@ -10,44 +11,36 @@ module Shifter #(parameter DWIDTH = 8)(
     output reg [DWIDTH-1:0] SPDR_out,
     output reg Data_out
 );
-reg [DWIDTH-1:0] shifter_data_reg;
-reg [DWIDTH-1:0] shifter_data;
-reg Data_in_sampled;
+    reg [DWIDTH-1:0] shifter_data_reg;
+    reg [DWIDTH-1:0] shifter_data;
 
-always@(posedge Sample_clk)
-begin
- Data_in_sampled <= Data_in;
-end
+    always@(posedge Sample_clk) begin
+        shifter_data_reg <= {shifter_data_reg[DWIDTH-2:0] ,Data_in};
+    end
 
 
-always@(posedge Shift_clk)
-begin
-if(shifter_en)
-begin
-Data_out <= shifter_data_reg[DWIDTH-1];
-shifter_data_reg <= {shifter_data_reg[DWIDTH-2:0] ,Data_in_sampled};
-end
-end
+    always@(posedge Shift_clk) begin
+        if(shifter_en) begin
+            Data_out <= shifter_data_reg[DWIDTH-1];  
+        end
+    end
 
 
-always@(*)
-begin
-if (SPDR_wr_en)
-begin
-shifter_data = SPDR_in;
-SPDR_out = 'bz ;
-end
-else if (SPDR_rd_en)
-begin
-SPDR_out = shifter_data ;
-shifter_data = shifter_data_reg;
-end
-else 
-begin
-shifter_data = shifter_data_reg;
-SPDR_out = 'bz ;
-end
-end
+    always@(*) begin
+        if (SPDR_wr_en) begin
+            SPDR_out = shifter_data ;
+            shifter_data = shifter_data_reg;
+        end
+        else if (SPDR_rd_en) begin
+            shifter_data_reg = SPDR_in;
+            shifter_data = SPDR_in;
+            SPDR_out = 'bz ;
+        end
+        else begin
+            shifter_data = shifter_data_reg;
+            SPDR_out = 'bz ;
+        end
+    end
 
 
 endmodule
---
+++ TestBench/TestBench.v
@@ -0,0 +1,7 @@
+`default_nettype none
+`timescale 1ns/100ps
+
+module SPI_TB;
+    reg [7:0] SPCR_in
+
+endmodule
